75653703cb2ad30f4e7d7636ddb5bbff086c9adf
Tasluf665 <tasluf15-12089@diu.edu.bd>
2022-12-26 21:11:52 +0600

    Implement into a new way

--- index.js
+++ index.js
@@ -8,64 +8,76 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 const getVideoUrl = async (url, chatId) => {
+  let videosName = [];
+  let videos = [];
   try {
     const info = await ytdl.getInfo(url);
-
-    for (let i = 0; i < info.formats.length; i++) {
-      if (
-        info.formats[i].hasAudio &&
-        info.formats[i].hasVideo &&
-        info.formats[i].qualityLabel === "720p"
-      ) {
-        bot.sendMessage(chatId, info.formats[i].url).catch((error) => {
-          console.log(error.message);
-          process.exit(1);
-        });
-        break;
+    info.formats.forEach((element) => {
+      if (element.hasVideo && element.hasAudio) {
+        videosName.push("/format " + element.qualityLabel);
+        videos.push(element);
       } else if (
-        info.formats[i].hasAudio &&
-        info.formats[i].hasVideo &&
-        info.formats[i].qualityLabel === "480p"
+        element.hasAudio &&
+        !element.hasVideo &&
+        element.mimeType.includes("audio/mp4")
       ) {
-        bot.sendMessage(chatId, info.formats[i].url).catch((error) => {
-          console.log(error.message);
-          process.exit(1);
-        });
-        break;
-      } else if (
-        info.formats[i].hasAudio &&
-        info.formats[i].hasVideo &&
-        info.formats[i].qualityLabel === "360p"
-      ) {
-        bot.sendMessage(chatId, info.formats[i].url).catch((error) => {
-          console.log(error.message);
-          process.exit(1);
-        });
-        break;
-      } else if (
-        info.formats[i].hasAudio &&
-        info.formats[i].hasVideo &&
-        info.formats[i].qualityLabel === "240p"
-      ) {
-        bot.sendMessage(chatId, info.formats[i].url).catch((error) => {
-          console.log(error.message);
-          process.exit(1);
-        });
-        break;
+        videosName.push("/format " + "mp3");
+        videos.push(element);
       }
-    }
+    });
+
+    bot
+      .sendMessage(chatId, "Choose video or audio format", {
+        reply_markup: {
+          keyboard: [videosName],
+        },
+      })
+      .then((res) => {
+        bot.onText(/\/format/, (msg) => {
+          const format = msg.text.replace("/format ", "");
+
+          videos.forEach((element) => {
+            if (format === "mp3" && element.qualityLabel === null) {
+              bot.sendMessage(chatId, element.url);
+              videosName = [];
+              videos = [];
+            } else if (element.qualityLabel === format) {
+              bot.sendMessage(chatId, element.url);
+              videosName = [];
+              videos = [];
+            }
+          });
+        });
+      });
   } catch (error) {
-    await bot.sendMessage(chatId, "No video found. Sorry");
+    bot.sendMessage(chatId, "Sorry no video found...");
+    console.log(error.message);
   }
 };
 
-bot.on("message", (msg) => {
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Welcome to YouTube video downloder");
+});
 
-  if (msg.text.includes("/start")) {
-    bot.sendMessage(chatId, "Welcome");
-  } else {
-    getVideoUrl(msg.text, chatId);
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(
+    chatId,
+    "Use the correct format to get the video. Example: /url https://www.youtube.com/watch?v=PKKibX_baoo"
+  );
+});
+
+bot.onText(/\/url/, async (msg) => {
+  const chatId = msg.chat.id;
+  const url = msg.text.replace("/url ", "");
+
+  await getVideoUrl(url, chatId);
+});
+
+bot.on("message", (msg) => {
+  if (!msg.text.includes("/")) {
+    bot.sendMessage(msg.chat.id, "Sorry I don't understand...");
   }
 });
 
